9056edd4e6ef902142fda7490e38da4c29b2cf97
irinesara <irine.ixa@gmail.com>
2025-01-23 12:52:21 +0530

    Config

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Route,Routes } from 'react-router-dom'
-import {Login} from './Components/login'
-import './App.css'
+
+import { Route, Routes } from 'react-router-dom'
+import { Login } from './Components/login'
+
 
 function App() {
 
+
   return (
     <>
-   <Routes>
-    <Route path="/login" elements={<Login/>}/>
-   </Routes>
+      <Routes>
+       <Route path='/login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
